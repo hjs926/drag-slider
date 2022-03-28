39cf68466e6baa2041cf7aa6002f3cf02418424f
hjs926 <joonsun926@gmail.com>
2022-03-29 01:09:41 +0900

    slide

---
+++ slider.js
@@ -0,0 +1,26 @@
+//Current DIV
+let current = 1;
+
+//Call by btn class
+const btnPre = document.querySelector(".pre"),
+  btnNext = document.querySelector(".next");
+
+//button Next
+function handleNextEvent() {
+  let target = document.querySelector(`.slider__0${current}`);
+  target.classList.remove("showing");
+  current = current + 1 < 6 ? current + 1 : 1;
+  target = document.querySelector(`.slider__0${current}`);
+  target.classList.add("showing");
+}
+btnNext.addEventListener("click", handleNextEvent);
+
+//button Pre
+function handlePreEvent() {
+  let target = document.querySelector(`.slider__0${current}`);
+  target.classList.remove("showing");
+  current = current - 1 > 0 ? current - 1 : 5;
+  target = document.querySelector(`.slider__0${current}`);
+  target.classList.add("showing");
+}
+btnPre.addEventListener("click", handlePreEvent);
